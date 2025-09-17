ad3fbdc0111e4c5b8831297f75a7520ba46597ac
Tiago Emanuel de Lima <tiagoepitanga10@gmail.com>
2025-09-17 13:26:35 -0300

    Fix bug in profile/index.tsx

--- packages/http-schemas/get-user-profile.ts
+++ packages/http-schemas/get-user-profile.ts
@@ -8,7 +8,7 @@ export const getUserProfileResponse = z.object({
     cpf: z.string().min(11).max(11),
     phone: z.string().min(10).max(11),
     gender: z.string().min(1).max(1),
-    date_of_birth: z.date(),
+    date_of_birth: z.string(),
     address: z.string().min(1).max(255),
     avatar_url: z.url().nullable().optional(),
     created_at: z.date(),
