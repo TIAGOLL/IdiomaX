b8963e032f4f4c1582729158c85068d24767f91e
Tiago Emanuel de Lima <tiagoepitanga10@gmail.com>
2025-10-13 12:47:03 -0300

    update

--- api/tsup.config.ts
+++ api/tsup.config.ts
@@ -5,4 +5,19 @@ export default defineConfig({
   splitting: false,
   sourcemap: true,
   clean: true,
+  format: ['esm'],
+  dts: true,
+  minify: false,
+  bundle: true,
+  outExtension: () => ({
+    js: '.js',
+  }),
+  esbuildOptions(options) {
+    options.platform = 'node'
+    options.format = 'esm'
+    options.outExtension = { '.js': '.js' }
+    options.banner = {
+      js: `import { createRequire } from 'module';const require = createRequire(import.meta.url);`,
+    }
+  },
 })
