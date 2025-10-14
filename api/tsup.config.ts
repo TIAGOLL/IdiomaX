0660d87710741d6b024b728944e20dd5a0a5a036
Tiago Emanuel de Lima <tiagoepitanga10@gmail.com>
2025-10-14 01:07:32 -0300

    udpate

--- api/tsup.config.ts
+++ api/tsup.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/http/server.ts'],
+  entry: ['src/**/*.ts'],
   format: ['esm'],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
+  bundle: true,
+  minify: false,
+  outExtension: () => ({
+    js: '.js'
+  }),
+  platform: 'node',
+  esbuildOptions(options) {
+    options.mainFields = ['module', 'main']
+    options.conditions = ['module']
+    options.format = 'esm'
+  }
 })
